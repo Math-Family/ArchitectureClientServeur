a524e686b00bd09181ce8d3bd7a1c4ecd63b4aff
sam1610 <samir.amri@gmail.com>
2021-03-27 22:02:50 +0300

    just  lab files only

--- envoi_svg.php
+++ envoi_svg.php
@@ -4,7 +4,7 @@ function envoiSvg( $w, $h) {
     header('Content-type: image/svg+xml');
     $chaine = "<!DOCTYPE svg PUBLIC '-//W3C//DTD SVG 1.1//EN' 'http://www.w3.org/Graphics/SVG/1.1/DTD/svg11.dtd'>";
     $chaine .='<svg width="100%" height="100%" xmlns="http://www.w3.org/2000/svg">
-    <rect x="50" y="20" rx="20" ry="20" width="150" height="150"
+    <rect x="50" y="20" rx="20"   ry="20" width="150" height="150"
     style="fill:red;stroke:black;stroke-width:5;opacity:0.5" />
     </svg>';
     return $chaine;
